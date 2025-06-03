6f9f36a313ece84564d9a78053a9674eaa109191
Morten Jensen <mo_ten@hotmail.com>
2025-06-03 21:12:57 +0200

    wip

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        new FlowerShop("Stalks and roots").runDialog();
+        new FlowerShop("Stalks and Roots").runDialog();
     }
 }
